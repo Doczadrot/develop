e5c11de28aa9586a23a7adc46d0c771f1ac67de2
Doczadrot <155732866+Doczadrot@users.noreply.github.com>
2024-01-17 20:08:50 +0300

    Create str_func.py
    "Add str_func.py with to_uppercase function".

---
+++ str_func.py
@@ -0,0 +1,11 @@
+def to_uppercase(input_str):
+    """
+    Converts the input string to uppercase.
+
+    Parameters:
+    input_str (str): The input string.
+
+    Returns:
+    str: The uppercase string.
+    """
+    return input_str.upper()
